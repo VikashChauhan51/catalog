f2934303cd38a4c2bef508ae3678150e41b26339
Vikash Chauhan <vikashchauhan51@gmail.com>
2024-06-17 16:19:17 +0530

    Fix: separate validators

--- src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -5,16 +5,6 @@ public record CreateProductCommand(string Name, List<string> Category, string De
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
-{
-    public CreateProductCommandValidator()
-    {
-        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
-        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
-        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
-        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
-    }
-}
 
 internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
--- src/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -4,13 +4,6 @@
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
 
-public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
-{
-    public DeleteProductCommandValidator()
-    {
-        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
-    }
-}
 
 internal class DeleteProductCommandHandler(IDocumentSession session)
     : ICommandHandler<DeleteProductCommand, DeleteProductResult>
